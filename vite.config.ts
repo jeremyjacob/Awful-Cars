9b497d767dce53160db0ee276f94e715ee88dd64
Jeremy Jacob <github@jeremyjacob.dev>
2021-09-20 02:30:10 -0700

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path'
 const resolve = (rel: string) => path.resolve(__dirname, rel)
 
 export default defineConfig({
+    build: {
+        outDir: './docs',
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
